a0f873fdf328b7fb34ca1bc38a34b4cfa410d826
Kacper Kubacki <kacper.kubacki87@gmail.com>
2023-10-22 22:56:47 +0200

    :sparkles: add controller

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
+use App\Repository\MeasurementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{id\d+}', name: 'app_weather')]
-    public function city(): Response
+    #[Route('/weather/{id}', name: 'app_weather', requirements: ['id' => '\d+'])]
+    public function city(Location $location, MeasurementRepository $repository): Response
     {
-        return $this->render('weather/index.html.twig', [
-            'controller_name' => 'WeatherController',
+        $measurements = $repository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
         ]);
     }
 }
--- src/Repository/MeasurementRepository.php
+++ src/Repository/MeasurementRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Location;
 use App\Entity\Measurement;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -19,6 +20,22 @@ class MeasurementRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Measurement::class);
+    }
+
+    public function findByLocation(Location $location)
+    {
+        $qb = $this->createQueryBuilder('m');
+
+        $qb->where('m.location = :location')
+            ->setParameter('location', $location)
+            ->andWhere('m.date > :now')
+            ->setParameter('now', date('Y-m-d'));
+
+        $query = $qb->getQuery();
+
+        $result = $query->getResult();
+
+        return $result;
     }
 
 //    /**
