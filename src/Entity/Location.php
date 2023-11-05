b6e247d36066c6365279ed3fbcedc7b1c49d7e24
Kacper Kubacki <kacper.kubacki87@gmail.com>
2023-11-05 20:43:26 +0100

    :sparkles: add lab 4

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -36,6 +36,11 @@ class Location
         $this->measurements = new ArrayCollection();
     }
 
+    public function __toString(): string
+    {
+        return $this->city;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
---
+++ src/Form/LocationType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('city', null, [
+                'attr' => [
+                    'placeholder' => 'Enter city name',
+                ],
+            ])
+            ->add('country', ChoiceType::class, [
+                'choices' => [
+                    'Poland' => 'PL',
+                    'Germany' => 'DE',
+                    'France' => 'FR',
+                    'Spain' => 'ES',
+                    'Italy' => 'IT',
+                    'United Kingdom' => 'GB',
+                    'United States' => 'US',
+                ],
+            ])
+            ->add('latitude')
+            ->add('longitude')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
